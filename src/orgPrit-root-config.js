fbae2b1bcdfb194127fa372ab37d1f3aa6702454
Pritam Halder <halderpritamhalder1@gmail.com>
2025-01-06 00:40:28 +0530

    app setup completed

--- src/orgPrit-root-config.js
+++ src/orgPrit-root-config.js
@@ -1,20 +1,38 @@
+import { constructRoutes, constructApplications, constructLayoutEngine } from "single-spa-layout";
 import { registerApplication, start } from "single-spa";
-import {
-  constructApplications,
-  constructRoutes,
-  constructLayoutEngine,
-} from "single-spa-layout";
-import microfrontendLayout from "./microfrontend-layout.html";
 
-const routes = constructRoutes(microfrontendLayout);
-const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return import(/* webpackIgnore: true */ name);
+// Fetch the layout template from the DOM
+const layoutElement = document.querySelector("#single-spa-layout");
+
+// Ensure layoutElement exists and is valid
+if (!layoutElement) {
+  throw new Error("Layout template not found in the DOM.");
+}
+
+// Construct routes and applications
+const routes = constructRoutes(layoutElement, {
+  loaders: {
+    topNav: "<h1>Loading topnav</h1>",
+  },
+  errors: {
+    topNav: "<h1>Failed to load topnav</h1>",
   },
 });
-const layoutEngine = constructLayoutEngine({ routes, applications });
 
+const applications = constructApplications({
+  routes,
+  loadApp: ({ name }) => System.import(name),
+});
+
+const layoutEngine = constructLayoutEngine({
+  routes,
+  applications,
+  active: false,
+});
+
+// Register applications
 applications.forEach(registerApplication);
+
+// Activate layout engine and start Single-SPA
 layoutEngine.activate();
 start();
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,106 @@
-const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
 
-module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "orgPrit";
-  const defaultConfig = singleSpaDefaults({
-    orgName,
-    projectName: "root-config",
-    webpackConfigEnv,
-    argv,
-    disableHtmlGeneration: true,
-  });
-
-  return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
-    plugins: [
-      new HtmlWebpackPlugin({
-        inject: false,
-        template: "src/index.ejs",
-        templateParameters: {
-          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
-          orgName,
+// Common configuration
+const commonConfig = {
+  entry: './src/orgPrit-root-config.js',
+  output: {
+    filename: 'orgPrit-root-config.js', // Use contenthash for production
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: false, // Minification handled by mode
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
-      }),
+      },
     ],
-  });
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: 'src/index.ejs',
+      templateParameters: {
+        isLocal: process.env.NODE_ENV === 'development',
+        orgName: 'orgPrit',
+      },
+    }),
+  ],
+};
+
+// Development configuration
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    static: path.join(__dirname, 'src'),
+    port: 9000,
+    historyApiFallback: true,
+    onBeforeSetupMiddleware: function (devServer) {
+      devServer.app.get('/orgPrit-root-config.js', (req, res) => {
+        const filePath = path.join(__dirname, 'dist', 'orgPrit-root-config.js');
+        res.sendFile(filePath);
+      });
+    },
+    
+    hot: true,
+    open: true,
+  },
+  // Source maps for easier debugging
+  devtool: 'eval-source-map',
+};
+
+// Production configuration
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: 'orgPrit-root-config.js', // Remove contenthash for predictable filename
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true, // Enable minification for production
+            },
+          },
+        ],
+      },
+    ],
+  },
+  // Optimization settings for production
+  optimization: {
+    minimize: true,
+  },
+};
+
+module.exports = (env) => {
+  if (env && env.production) {
+    return merge(commonConfig, prodConfig);
+  }
+  return merge(commonConfig, devConfig);
 };
